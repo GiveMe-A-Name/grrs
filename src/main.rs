918b02339e90b9326d4d9e481a38b29ecd31691b
GiveMe-A-Name <a455300764b@hotmail.com>
2022-04-19 19:53:15 +0800

    feat(cli): parse cli command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{:?}", args);
 }
