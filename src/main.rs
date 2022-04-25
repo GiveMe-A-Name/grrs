fd69ed5ecf195c96f53c33a24a29b741331c6065
GiveMe-A-Name <a455300764b@hotmail.com>
2022-04-25 12:47:31 +0800

    fix(global): remove anyhow dependency

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::{Context, Error};
 use clap::Parser;
 use grrs::find_matches;
 use std::fs;
@@ -11,12 +10,12 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let stdout = io::stdout();
     let mut handle = io::BufWriter::new(stdout.lock());
     let args = Cli::parse();
     let content = fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file {:?}", &args.path))?;
+        .map_err(|_| format!("could not read file {:?}", &args.path))?;
 
     find_matches(&content, &args.pattern, &mut handle);
     Ok(())
